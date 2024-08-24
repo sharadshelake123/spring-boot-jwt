e6c5d7932aec943d2a341d30df5f8cbdde2a93a5
sharad shelake <sharadshelake123@gmail.com>
2024-08-24 20:07:08 +0530

    initial commit

---
+++ src/main/java/com/jwt/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.jwt.repository;
+
+import com.jwt.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
